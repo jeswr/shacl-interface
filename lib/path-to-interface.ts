7ca7f43c0ed5661e863bb4c5f87da7b4382d69f6
jeswr <jesse.wright@anu.edu.au>
2021-01-17 23:09:17 +1100

    WIP

--- lib/path-to-interface.ts
+++ lib/path-to-interface.ts
@@ -1,9 +1,9 @@
-import {} from 'ts-morph';
+import { InterfaceDeclaration } from 'ts-morph';
 import type { ProxiedResource } from 'rdf-object-proxy';
 
 function pathToInterface(path: ProxiedResource<string>) {
   if (path.type === 'NamedNode') {
-    
+    return new InterfaceDeclaration();
   }
 }
 
